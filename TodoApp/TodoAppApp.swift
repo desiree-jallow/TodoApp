26ba97f815ffdd08d33e9c454dbdca9bf14ac88b
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-26 15:40:47 -0500

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Desiree on 4/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
